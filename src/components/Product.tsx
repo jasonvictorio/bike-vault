0dc9d9bb5f793446a602fa9fc4293929c6a264bf
Jason Victorio <jgvictorio@gmail.com>
2020-08-11 05:34:09 +1200

    show image in product catalog

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from '@reach/router'
 
-const Product = ({ product }: { product: any }) => {
+import { Product as ProductType } from '../types'
+
+const Product = ({ product }: { product: ProductType }) => {
   return (
     <Link to={`/product/${product.slug}`} className='leading-snug'>
       <div className='relative h-0' style={{ paddingBottom: '100%' }}>
-        {/* <img className='absolute h-full object-contain top-0 w-full' src={require(`../../images/${product.image}`).default} ></img> */}
+        <img className='absolute h-full object-contain top-0 w-full' src={`/assets/images/${product.image}`}></img>
       </div>
       <h1>{product.name}</h1>
       <div className='text-gray-600'>

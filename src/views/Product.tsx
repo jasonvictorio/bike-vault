af298c77a92cd70fed194f7c93753f56a22acf23
Jason Victorio <jgvictorio@gmail.com>
2020-08-11 05:52:17 +1200

    update product view

--- src/views/Product.tsx
+++ src/views/Product.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { RouteComponentProps } from '@reach/router'
 import { useSelector, useDispatch } from 'react-redux'
 import { find, isNil } from 'lodash'
@@ -17,10 +17,39 @@ const Product = ({ slug }: RouteComponentProps<{ slug: string }>) => {
 
   return (
     <div>
-      <button className='border px-3 py-2' onClick={handleAddToCart}>
-        Add to Cart
-      </button>
-      <pre>{JSON.stringify(product, null, 2)}</pre>
+      {product && (
+        <div className='container'>
+          <h1 className='text-3xl font-black leading-snug'>{product.name}</h1>
+          <div className='text-gray-600'>
+            {product.brand} {product.year}
+          </div>
+          <div>
+            <img src={`/assets/images/${product.image}`} alt={`${product.brand} ${product.name} ${product.year}`} />
+          </div>
+          <div className='text-4xl leading-snug flex items-center py-5'>
+            {product.discountedPrice ? (
+              <>
+                <div className='mr-2'>${product.discountedPrice}</div>{' '}
+                <div className='text-2xl line-through text-gray-600'>${product.price}</div>
+              </>
+            ) : (
+              <div>${product.price}</div>
+            )}
+          </div>
+          <button className='border border-black block w-full px-2 py-3 text-lg' onClick={handleAddToCart}>
+            Add to cart
+          </button>
+          <h1 className='text-2xl font-black mt-10 mb-6'>Specifications</h1>
+          <dl className='leading-normal'>
+            {product.specifications?.map(specification => (
+              <>
+                <dt className='text-gray-600 mb-2'>{specification.name}</dt>
+                <dd className='mb-6'>{specification.description}</dd>
+              </>
+            ))}
+          </dl>
+        </div>
+      )}
     </div>
   )
 }

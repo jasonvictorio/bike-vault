a3e31a1d26f1287f8aa637877d061e6e2cad431f
Jason Victorio <jgvictorio@gmail.com>
2020-08-12 06:28:54 +1200

    update category view

--- src/views/Category.tsx
+++ src/views/Category.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
-import ProductCatalog from '../components/ProductCatalog'
 import { useSelector } from 'react-redux'
-import { selectByCategories } from '../store/products'
 import { RouteComponentProps } from '@reach/router'
+import { isNil, compact } from 'lodash'
 
-interface props {
-  category: string
-  subCategory: string
+import Breadcrumbs from '../components/Breadcrumbs'
+import ProductCatalog from '../components/ProductCatalog'
+import { selectByCategories } from '../store/products'
+import { slugToString } from '../utils'
+import { Slug } from '../types'
+
+interface props extends RouteComponentProps {
+  category?: Slug
+  subCategory?: Slug
 }
 
-const Category = ({ category = '', subCategory = '' }: RouteComponentProps<props>) => {
-  const products = useSelector(selectByCategories([category, subCategory]))
+const Category = ({ category, subCategory }: props) => {
+  const categoryString = slugToString(category ?? '')
+  const subCategoryString = slugToString(subCategory ?? '')
 
-  return <ProductCatalog products={products} />
+  const products = useSelector(selectByCategories(compact([categoryString, subCategoryString])))
+  const title = isNil(subCategory) ? categoryString : subCategoryString
+
+  return (
+    <>
+      <div className='container'>
+        <h1 className='text-3xl font-black leading-snug'>{title}</h1>
+        <Breadcrumbs category={category} subCategory={subCategory} />
+      </div>
+      <ProductCatalog products={products} />
+    </>
+  )
 }
 
 export default Category
